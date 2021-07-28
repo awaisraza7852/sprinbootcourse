22c8e65a31e30b4d4a2a70bd072e4ec2a5940ed1
Muhammad Awais Gujjar <raxa7852@gmail.com>
2021-07-28 12:06:01 +0500

    model and view objects

--- src/main/java/com/example/mywebapp/coursescontroller.java
+++ src/main/java/com/example/mywebapp/coursescontroller.java
@@ -3,7 +3,8 @@ package com.example.mywebapp;
 import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -13,11 +14,15 @@ import javax.ws.rs.Produces;
 @Controller
 public class coursescontroller {
     @RequestMapping("/courses")
-    public String courses(HttpServletRequest req){
-        String coursename = req.getParameter(("cname"));
-        HttpSession session= req.getSession();
-        session.setAttribute("cname",coursename);
-        return "courses";
+    public ModelAndView courses(@RequestParam("cname")String coursename ){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("cname",coursename);
+        mv.setViewName("courses");
+        return mv;
+        //String coursename = req.getParameter(("cname"));
+        //HttpSession session= req.getSession();
+        //session.setAttribute("cname",coursename);
+       // return "courses";
     }
 
 }
